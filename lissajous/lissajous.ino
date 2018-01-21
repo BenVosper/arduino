76036bddabff04ab6d6933261355b4c0b2f60f9b
Ben Vosper <ben-vosper@hotmail.com>
2018-01-21 16:21:13 +0000

    Use TaskScheduler

--- lissajous/lissajous.ino
+++ lissajous/lissajous.ino
@@ -1,28 +1,64 @@
+#define _TASK_MICRO_RES
 #include <TaskScheduler.h>
 
-const int y = 5;
-const int x = 6;
+short y = 5;
+short x = 6;
 
-int get_analogue_value(float pos) {
-  return (int) (255 * constrain(pos, 0, 1));
+byte x_max = 255;
+byte y_max = 255;
+
+const int fps = 60;
+const long period = (long) 1000000 / fps;
+
+
+void draw(byte x_pos, byte y_pos) {
+  analogWrite(x, x_pos);
+  analogWrite(y, y_pos);
+//  Serial.print(x_pos);
+//  Serial.print("\t");
+//  Serial.print(y_pos);
+//  Serial.println("");
   }
 
-
-void draw(float x_pos, float y_pos) {
-  analogWrite(x, get_analogue_value(x_pos));
-  analogWrite(y, get_analogue_value(y_pos));
+void buzz() {
+  byte x_pos = random(x_max);
+  byte y_pos = random(y_max);
+  draw(x_pos, y_pos);
   }
 
-void buzz(float x_max = 1, float y_max = 1) {
-  draw(random(x_max), random(y_max));
-  }
-  
+void buzzball() {
+  static byte ball_x_pos;
+  static byte ball_y_pos;
 
+  byte ball_radius = 5;
+
+  if (random(2)) {
+    ball_x_pos = ball_x_pos + 1;
+  } else {
+    ball_x_pos = ball_x_pos - 1;
+  }
+
+  if (random(2)) {
+    ball_y_pos = ball_y_pos + 1;
+  } else {
+    ball_y_pos = ball_y_pos - 1;
+  }
+
+  byte x_pos = random(ball_x_pos - ball_radius, ball_x_pos + ball_radius);
+  byte y_pos = ball_y_pos + (byte) sqrt(sq(ball_radius) - sq(x_pos));
+
+  draw(x_pos, y_pos);
+  }
+
+Task draw_task(period, -1, buzzball);
+Scheduler task_scheduler;
+ 
 void setup() {
-  
+//  Serial.begin(9600);
+  task_scheduler.addTask(draw_task);
+  draw_task.enable();
 }
 
 void loop() {
-  buzz();
-
+  task_scheduler.execute();
 }
